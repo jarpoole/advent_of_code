7f327b704267eb4f07ca84ebc24699f3d12fdf5d
Jared Poole <43940867+jarpoole@users.noreply.github.com>
2025-12-20 17:55:39 -0700

    building out boilerplate generator

---
+++ gen_boilerplate.rs
@@ -0,0 +1,22 @@
+use git2::{Repository, Status, StatusOptions};
+
+fn git_repo_has_uncommitted_changes(repo: &Repository) -> Result<bool, git2::Error> {
+    let mut options = StatusOptions::new();
+    options.include_untracked(true).recurse_untracked_dirs(true);
+    Ok(repo
+        .statuses(Some(&mut options))?
+        .iter()
+        .any(|file| file.status() == Status::CURRENT))
+}
+
+fn main() {
+    let year = std::env::args()
+        .nth(1)
+        .expect("year should be provided as the first argument");
+    let day = std::env::args()
+        .nth(2)
+        .expect("day should be provided as the second argument");
+
+    let repo = Repository::init(".").expect("should always be run in the repository root");
+    println!("repo state: {:?}", git_repo_has_uncommitted_changes(&repo));
+}
