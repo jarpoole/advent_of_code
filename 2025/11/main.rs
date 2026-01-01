bbca6587309377cd4292f270fcee51ff8fb216c0
Jared Poole <43940867+jarpoole@users.noreply.github.com>
2026-01-01 18:09:18 -0500

    day 11 part 2 solution

--- 2025/11/main.rs
+++ 2025/11/main.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2025/day/11
 
-use itertools::Itertools;
+use itertools::{FoldWhile, Itertools};
 use std::{
     collections::{HashMap, HashSet},
     hash::Hash,
@@ -21,8 +21,11 @@ impl<'a> Device<'a> {
     }
 }
 
-static INITIAL_DEVICE: Device = Device::new("you");
-static TARGET_DEVICE: Device = Device::new("out");
+struct Constraints<'a> {
+    from: Device<'a>,
+    to: Device<'a>,
+    including: HashSet<Device<'a>>,
+}
 
 type DeviceInputAdjacencyList<'a> = HashMap<Device<'a>, Vec<Device<'a>>>;
 
@@ -52,47 +55,202 @@ fn parse_device_inputs(input: &str) -> DeviceInputAdjacencyList<'_> {
 // When determining the visit count for any given node there are several possibilities
 //  1. No input node paths include any of the desired nodes
 //      ∴ visit count = sum all input visit counts
-//  2. Exactly one input node path includes one or more desired nodes
+//  2. Exactly one input node path includes one or more desired nodes but not all of them
 //      ∴ visit count = only this input node visit count
-//  3. More than one input node path includes the desired node
+//  3. More than one input node path includes some of the desired nodes but not all of them
 //      ∴ visit count = 0
-// This works because each node is guaranteed to be unique so if
-fn count_paths(device_inputs: DeviceInputAdjacencyList) -> u64 {
-    let mut stack = vec![(TARGET_DEVICE)];
-    let mut visit_counts: HashMap<Device, u64> = HashMap::new();
+//  4. More than one input node path includes some of the desired nodes and some of the input
+//     include all of them
+//      ∴ visit count = only the input paths that include all the desired nodes
+// This works because each node is guaranteed to be unique so if different required nodes
+// appear in separate branches and the graph is acyclic then no path can contain both
+#[derive(PartialEq)]
+enum InputVisitState<'a> {
+    MissingDependencies,
+    AllConstraintsSatisfied(u64),
+    SomeConstraintsSatisfied(u64, HashSet<Device<'a>>),
+    NoConstraintsSatisfied(u64),
+    ConflictingConstraints,
+}
+fn count_paths(device_inputs: DeviceInputAdjacencyList, constraints: Constraints) -> u64 {
+    let mut stack = vec![constraints.to];
+    let mut visit_counts: HashMap<Device, (u64, Option<HashSet<Device>>)> = HashMap::new();
     while let Some(device) = stack.pop() {
         let inputs = device_inputs.get(&device).expect(&format!(
             "all devices should be in the adjacency list including {:?}",
             device
         ));
 
-        if let Some(input_visit_counts) = inputs
+        if device == constraints.from {
+            visit_counts.insert(device, (1, None));
+            continue;
+        }
+
+        let visit_count = inputs
             .iter()
             .map(|input| visit_counts.get(input))
-            .fold_while(Some(0), |acc, curr| match curr {
-                Some(current_visit_count) => {
-                    itertools::FoldWhile::Continue(acc.map(|accumulated_visit_count| {
-                        accumulated_visit_count + current_visit_count
-                    }))
-                }
-                None => itertools::FoldWhile::Done(None),
-            })
-            .into_inner()
-        {
-            if device == INITIAL_DEVICE {
-                visit_counts.insert(device, 1);
-            } else {
-                visit_counts.insert(device, input_visit_counts);
+            .fold_while(
+                InputVisitState::NoConstraintsSatisfied(0),
+                |acc, curr| match curr {
+                    Some((current_input_visit_count, v)) => {
+                        match acc {
+                            // check if one or more input paths already satisfy all constraints
+                            InputVisitState::AllConstraintsSatisfied(accumulated_visit_count) => {
+                                if let Some(current_visited_constraints) = v
+                                    && *current_visited_constraints == constraints.including
+                                {
+                                    // more than one input has paths which already fully satisfy constraints
+                                    // so sum them
+                                    return FoldWhile::Continue(
+                                        InputVisitState::AllConstraintsSatisfied(
+                                            accumulated_visit_count + current_input_visit_count,
+                                        ),
+                                    );
+                                } else {
+                                    // discard the result if the current input paths
+                                    return FoldWhile::Continue(acc);
+                                }
+                            }
+                            // check if one or more input paths satisfy some of the constraints
+                            InputVisitState::SomeConstraintsSatisfied(
+                                accumulated_visit_count,
+                                accumulated_visited_constraints,
+                            ) => {
+                                if let Some(current_visited_constraints) = v
+                                    && *current_visited_constraints == constraints.including
+                                {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::AllConstraintsSatisfied(
+                                            *current_input_visit_count,
+                                        ),
+                                    );
+                                } else if let Some(current_visited_constraints) = v
+                                    && *current_visited_constraints
+                                        == accumulated_visited_constraints
+                                {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::SomeConstraintsSatisfied(
+                                            accumulated_visit_count + current_input_visit_count,
+                                            accumulated_visited_constraints,
+                                        ),
+                                    );
+                                // if multiple paths each have different partial constraints then neither can
+                                // ever be completed
+                                } else if let Some(current_visited_constraints) = v
+                                    && *current_visited_constraints
+                                        != accumulated_visited_constraints
+                                {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::ConflictingConstraints,
+                                    );
+                                // discard the current input as it can never be complete given it is missing
+                                // at least one constraint other input paths have already passed through
+                                } else {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::SomeConstraintsSatisfied(
+                                            accumulated_visit_count,
+                                            accumulated_visited_constraints.clone(),
+                                        ),
+                                    );
+                                }
+                            }
+                            InputVisitState::NoConstraintsSatisfied(accumulated_visit_count) => {
+                                if let Some(current_visited_constraints) = v
+                                    && *current_visited_constraints == constraints.including
+                                {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::AllConstraintsSatisfied(
+                                            *current_input_visit_count,
+                                        ),
+                                    );
+                                } else if let Some(current_visited_constraints) = v {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::SomeConstraintsSatisfied(
+                                            *current_input_visit_count,
+                                            current_visited_constraints.clone(),
+                                        ),
+                                    );
+                                } else {
+                                    return FoldWhile::Continue(
+                                        InputVisitState::NoConstraintsSatisfied(
+                                            *current_input_visit_count + accumulated_visit_count,
+                                        ),
+                                    );
+                                }
+                            }
+                            InputVisitState::ConflictingConstraints => FoldWhile::Continue(acc),
+                            InputVisitState::MissingDependencies => {
+                                unimplemented!("fold while will always returns early")
+                            }
+                        }
+                    }
+                    None => FoldWhile::Done(InputVisitState::MissingDependencies),
+                },
+            )
+            .into_inner();
+
+        match visit_count {
+            InputVisitState::AllConstraintsSatisfied(input_visit_counts) => {
+                visit_counts.insert(
+                    device,
+                    (input_visit_counts, Some(constraints.including.clone())),
+                );
             }
-        } else {
-            let unresolved_inputs = inputs
-                .iter()
-                .filter(|device| !visit_counts.contains_key(device));
-            stack.push(device);
-            stack.extend(unresolved_inputs);
+            InputVisitState::SomeConstraintsSatisfied(
+                input_visit_counts,
+                mut visited_constraints,
+            ) => {
+                if constraints.including.contains(&device) {
+                    visited_constraints.insert(device);
+                }
+                visit_counts.insert(device, (input_visit_counts, Some(visited_constraints)));
+            }
+            InputVisitState::NoConstraintsSatisfied(input_visit_counts) => {
+                if constraints.including.contains(&device) {
+                    visit_counts
+                        .insert(device, (input_visit_counts, Some(HashSet::from([device]))));
+                } else {
+                    visit_counts.insert(device, (input_visit_counts, None));
+                }
+            }
+            InputVisitState::ConflictingConstraints => {
+                visit_counts.insert(device, (0, None));
+            }
+            InputVisitState::MissingDependencies => {
+                let unresolved_inputs = inputs
+                    .iter()
+                    .filter(|device| !visit_counts.contains_key(device));
+                stack.push(device);
+                stack.extend(unresolved_inputs);
+            }
+        }
+        if device == Device::new("fff") {
+            dbg!(&visit_counts);
         }
     }
-    return *visit_counts.get(&TARGET_DEVICE).expect("");
+    return (*visit_counts.get(&constraints.to).expect("")).0;
+}
+
+fn count_part1_paths(device_inputs: DeviceInputAdjacencyList) -> u64 {
+    count_paths(
+        device_inputs,
+        Constraints {
+            from: Device::new("you"),
+            to: Device::new("out"),
+            including: HashSet::new(),
+        },
+    )
+}
+
+fn count_part2_paths(device_inputs: DeviceInputAdjacencyList) -> u64 {
+    count_paths(
+        device_inputs,
+        Constraints {
+            from: Device::new("svr"),
+            to: Device::new("out"),
+            including: HashSet::from([Device::new("fft"), Device::new("dac")]),
+        },
+    )
 }
 
 fn main() {
@@ -100,6 +258,10 @@ fn main() {
     let hashmap = parse_device_inputs(&input);
     println!(
         "The number of paths between 'you' and 'out' is: {}",
-        count_paths(hashmap.clone())
+        count_part1_paths(hashmap.clone())
+    );
+    println!(
+        "The number of paths between 'svr' and 'out' which pass through 'dac' and 'fft' is: {}",
+        count_part2_paths(hashmap)
     );
 }
--- 2025/11/tests.rs
+++ 2025/11/tests.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2025/day/11
 
-static EXAMPLE_INPUT: &str = r#"
+static EXAMPLE_INPUT_PART1: &str = r#"
     aaa: you hhh
     you: bbb ccc
     bbb: ddd eee
@@ -13,15 +13,32 @@ static EXAMPLE_INPUT: &str = r#"
     iii: out
 "#;
 
+static EXAMPLE_INPUT_PART2: &str = r#"
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out
+"#;
+
 use super::*;
 
 #[test]
 fn part1_example() {
-    let device_inputs = parse_device_inputs(EXAMPLE_INPUT);
-    assert_eq!(count_paths(device_inputs), 5);
+    let device_inputs = parse_device_inputs(EXAMPLE_INPUT_PART1);
+    assert_eq!(count_part1_paths(device_inputs), 5);
 }
 
 #[test]
 fn part2_example() {
-    assert_eq!(true, true);
+    let device_inputs = parse_device_inputs(EXAMPLE_INPUT_PART2);
+    assert_eq!(count_part2_paths(device_inputs), 2);
 }
